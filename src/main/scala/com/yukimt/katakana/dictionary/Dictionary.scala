76b5dacde87b2d3802aa3d785b18cfc23ebc9319
vagrant <vagrant@localhost.localdomain>
2016-08-14 00:38:01 +0000

    add convert function

--- src/main/scala/com/yukimt/katakana/dictionary/Dictionary.scala
+++ src/main/scala/com/yukimt/katakana/dictionary/Dictionary.scala
@@ -1,12 +1,12 @@
 package com.yukimt.katakana.dictionary
 
 import scala.io.Source
-import java.util.UUID
 /**
  * Able to convert strings to Katakana by using dictionary information.
  * Initially, read dictionary information from text files,
  * and save the information as Index
  *
+ * "word" in dictionary is case-insensitive
  * regular expression is usable in dictionary
  * regular expression is enclosed by "/"
  * if you want to use "," in the left part, escape with "\" such as a\,b,エーカンマビー
@@ -21,12 +21,17 @@ import java.util.UUID
  */
 abstract class Dictionary(userDictionaryPath: Option[String] = None){
   val dicFilePath = getClass.getResource("/katakana.dic").toURI.toString
-  
+  val unUsedStr = ";:+91qaxsw2"
+  val katakanaSet = ('ァ' to 'ン').toSet
+
+
   //read dictionary information from text file
   def readDictionary(filePath: String) = {
     // convert each line of the file into Index
-    val indices = Source.fromFile(dicFilePath).getLines.map{ line =>
+    val indices = Source.fromFile(filePath).getLines.map{ line =>
       val (term, reading) = splitLine(line)
+      if(!reading.forall(r => katakanaSet contains r))
+        throw new RuntimeException("reading of dictionary needs to be all Katakana")
       val letter = getLetter(term)
       Index(letter, term, reading)
     }
@@ -39,9 +44,9 @@ abstract class Dictionary(userDictionaryPath: Option[String] = None){
 
   //split line to Term and Reading
   protected def splitLine(line: String): (Term, Reading) = {
-    val uuid = UUID.randomUUID.toString
     //split by ',', but not split by '\,'
-    val splitted = line.replaceAll("\\\\,", uuid).split(",").map(_.replaceAll(uuid, ","))
+    val splitted = line.replaceAll("\\\\,", unUsedStr).split(",")
+      .map(_.replaceAll(unUsedStr, ","))
 
     if(splitted.length != 2) throw new RuntimeException(s"The format of the dictionary is wrong.\n WrongLine: $line")
     val Array(term, reading) = splitted
@@ -55,26 +60,56 @@ abstract class Dictionary(userDictionaryPath: Option[String] = None){
     if(term.length > 1 && term.head == '/' && term.last == '/'){
       // if regular expression is formed with /^.../, regard the third letter as the first letter
       if(term.length > 3 && term(1) == '^'){
-        if(term(2).toString.matches("[a-z]")) term(2)
-        else OtherLetter
+        if(isAlphabet(term(2))) term(2)
+        else KanjiLetter
       }
       else RELetter
     } else {
-      if(term.head.toString.matches("[a-z]")) term.head
-      else OtherLetter
+      if(isAlphabet(term.head)) term.head
+      else KanjiLetter
     }
   }
   
-  //save index to some data source (e.g. Redis, memcache, ...)
-  protected def save(index: Map[Char, Map[Term, Reading]]): Unit
-  
+ 
+  //initially read default and user dictionary
   readDictionary(dicFilePath)
   userDictionaryPath.foreach(u => readDictionary(u))
-
 
 
   /**
    * convert word into Katakana based on the index 
    */
-  def convert(word: String): String
+  def convert(word: String) = {
+    if(isAlphabet(word.head)){
+      val replacedWord:String = getRE.foldLeft(word.toLowerCase){(w, i) =>
+        w.replaceAll(i.term, i.reading)
+      }
+      val splittedWord = replacedWord.foldLeft(Array("")){(acc, c) => 
+        if(isAlphabet(acc.last.last) == isAlphabet(c))
+          acc.dropRight(1) :+ (acc.last + c)
+        else
+          acc :+ c.toString
+      }
+      splittedWord.map{ w =>
+        if(isAlphabet(w.head)) getEnglish(w.head).get(w).getOrElse(w)
+        else w
+      }.mkString
+    } else {
+      getKanji.foldLeft(word){(w, i) =>
+        w.replaceAll(i.term, i.reading)
+      }
+    }
+  }
+
+  protected def isAlphabet(c: Char) = c.toString.toLowerCase.matches("[a-z]")
+
+  //get Regular Expressions from saved dictionary information
+  protected def getRE: Seq[Index]
+  //get Kanji from saved dictionary information
+  protected def getKanji: Seq[Index]
+  //get English from saved dictionary information
+  protected def getEnglish(letter: Char): Map[Term, Reading]
+  
+  //save index to some data source (e.g. Redis, memcache, ...)
+  protected def save(index: Map[Char, Map[Term, Reading]]): Unit
 }
--- src/main/scala/com/yukimt/katakana/dictionary/package.scala
+++ src/main/scala/com/yukimt/katakana/dictionary/package.scala
@@ -4,7 +4,7 @@ package object dictionary{
   type Term = String
   type Reading = String
 
-  val OtherLetter = '#' // letter for non-alphabet
+  val KanjiLetter = '#' // letter for Kanji
   val RELetter = '/' // letter for regular expression
   case class Index(letter: Char, term: Term, reading: Reading)
 }
