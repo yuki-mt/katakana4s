14e630718922167208cfa484b42bd5e845fec7a0
yuki-mt <yuki_mt@volare.jp>
2016-08-30 13:11:48 +0900

    add convert method in Roman

--- src/main/scala/com/yukimt/katakana/alphabet/Roman.scala
+++ src/main/scala/com/yukimt/katakana/alphabet/Roman.scala
@@ -1,7 +1,48 @@
 package com.yukimt.katakana.alphabet
 
-object Roman extends Alphabet{
+case class Roman(userConsonants: Option[Map[String, Seq[String]]] = None) extends Alphabet{
   val vowels = Set('a', 'i', 'u', 'e', 'o')
+
+  val baseConsonants = Map(
+    "" -> Seq("ア", "イ", "ウ", "エ", "オ"),
+    "b"-> Seq("バ", "ビ", "ブ", "べ", "ボ"),
+    "c" -> Seq("カ", "キ", "ク", "ケ", "コ"),
+    "d" -> Seq("ダ", "ヂ", "ヅ", "デ", "ド"),
+    "f" -> Seq("ファ", "フィ", "フ", "フェ", "フォ"),
+    "g" -> Seq("ガ", "ギ", "グ", "ゲ", "ゴ"),
+    "h" -> Seq("ハ", "ヒ", "フ", "ヘ", "ホ"),
+    "j" -> Seq("ジャ", "ジ", "ジュ", "ジェ", "ジョ"),
+    "k" -> Seq("カ", "キ", "ク", "ケ", "コ"),
+    "l" -> Seq("ラ", "リ", "ル", "レ", "ロ"),
+    "m" -> Seq("マ", "ミ", "ム", "メ", "モ"),
+    "n" -> Seq("ナ", "ニ", "ヌ", "ネ", "ノ"),
+    "p" -> Seq("パ", "ピ", "プ", "ペ", "ポ"),
+    "q" -> Seq("クァ", "クィ", "クゥ", "クェ", "クォ"),
+    "r" -> Seq("ラ", "リ", "ル", "レ", "ロ"),
+    "s" -> Seq("サ", "シ", "ス", "セ", "ソ"),
+    "t" -> Seq("タ", "チ", "ツ", "テ", "ト"),
+    "v" -> Seq("ヴァ", "ヴィ", "ヴ", "ヴェ", "ヴォ"),
+    "w" -> Seq("ワ", "ウィ", "ウ", "ウェ", "ヲ"),
+    "x" -> Seq("ァ", "ィ", "ゥ", "ェ", "ォ"),
+    "y" -> Seq("ヤ", "イ", "ユ", "イェ", "ヨ"),
+    "z" -> Seq("ザ", "ジ", "ズ", "ゼ", "ゾ"),
+    "dy" -> Seq("ヂャ", "ヂィ", "ヂュ", "ヂェ", "ヂョ"),
+    "gy" -> Seq("ギャ", "ギィ", "ギュ", "ギェ", "ギョ"),
+    "hy" -> Seq("ヒャ", "ヒィ", "ヒュ", "ヒェ", "ヒョ"),
+    "ky" -> Seq("キャ", "キィ", "キュ", "キェ", "キョ"),
+    "my" -> Seq("ミャ", "ミィ", "ミュ", "ミェ", "ミョ"),
+    "ny" -> Seq("ニャ", "ニィ", "ニュ", "ニェ", "ニョ"),
+    "py" -> Seq("ピャ", "ピィ", "ピュ", "ピェ", "ピョ"),
+    "ry" -> Seq("リャ", "リィ", "リュ", "リェ", "リョ"),
+    "sy" -> Seq("シャ", "シィ", "シュ", "シェ", "ショ"),
+    "ty" -> Seq("チャ", "チィ", "チュ", "チェ", "チョ"),
+    "zy" -> Seq("ジャ", "ジィ", "ジュ", "ジェ", "ジョ"),
+    "sh" -> Seq("シャ", "シ", "シュ", "シェ", "ショ"),
+    "ch" -> Seq("チャ", "チ", "チュ", "チェ", "チョ"),
+    "ts" -> Seq("ツァ", "ツィ", "ツ", "ツェ", "ツォ"))
+
+  lazy val consonants = userConsonants.map(c => baseConsonants ++ c).getOrElse(baseConsonants)
+  lazy val mnSet = consonants.keys.filter(k => k.startsWith("m") || k.startsWith("n")).toSet
 
   def decompose(str: String): Array[Sound] = {
     str.headOption.map{ c =>
@@ -9,14 +50,53 @@ object Roman extends Alphabet{
         Sound(None, Some(c.toString)) +: decompose(str.tail)
       else{
         val sounds = decompose(str.tail)
-        if(sounds.isEmpty)
-          throw new IllegalArgumentException(s"Roman word needs to finish vowel: $str")
-        val consonant = c + sounds.head.consonant.getOrElse("")
-        sounds(0) = sounds.head.copy(consonant = Some(consonant))
-        sounds
+        if(sounds.isEmpty){
+          if(c == 'm' || c == 'n')
+            Array(Sound(Some(c.toString), None))
+          else
+            throw new IllegalArgumentException(s"Roman word needs to finish vowel: $str")
+        } else {
+          val consonant = c + sounds.head.consonant.getOrElse("")
+          if((mnSet contains consonant) || (c != 'm' && c != 'n')){
+            sounds(0) = sounds.head.copy(consonant = Some(consonant))
+            sounds
+          } else {
+            Sound(Some(c.toString), None) +: sounds
+          }
+        }
       }
     }.getOrElse(Array.empty[Sound])
   }
 
-  def convert(sounds: Seq[Sound]): String = ""
+  def convert(sounds: Seq[Sound]): String = {
+    sounds.map{s =>
+      val con = s.consonant.getOrElse("")
+      val (sokuon, consonant) = getSokuon(con)
+      sokuon + s.vowel.map{ v =>
+        consonants.get(consonant).map(seq => seq(vowelToInt(v.head))) match {
+          case Some(k) => k
+          case None => throw new IllegalArgumentException(s"the consonant is not in expected: $con")
+        }
+      }.getOrElse{
+        if(consonant == "m" || consonant == "n") "ン"
+        else throw new IllegalArgumentException(s"Sound needs both consonant and vowel: $s")
+      }
+    }.mkString
+  }
+
+  def getSokuon(con: String): (String, String) = {
+    if(con.length > 1 && con.head == con(1)){
+      val s = getSokuon(con.tail)
+      ("ッ" + s._1, s._2)
+    }
+    else ("", con)
+  }
+
+  def vowelToInt(vowel: Char) = vowel match {
+    case 'a' => 0
+    case 'i' => 1 
+    case 'u' => 2
+    case 'e' => 3
+    case 'o' => 4
+  }
 }
--- src/test/scala/com/yukimt/katakana/alphabet/RomanSpec.scala
+++ src/test/scala/com/yukimt/katakana/alphabet/RomanSpec.scala
@@ -5,8 +5,24 @@ import org.specs2.mutable.Specification
 class RomanSpec extends Specification{
   "Roman" should {
     "decompose" in {
-      Roman.decompose("otoshimae") === Array(Sound(None, Some("o")), Sound(Some("t"), Some("o")), Sound(Some("sh"), Some("i")),
-        Sound(Some("m"), Some("a")), Sound(None, Some("e")))
+      val r = Roman()
+      r.decompose("otoshimae") === Array(Sound(None, Some("o")), Sound(Some("t"), Some("o")), Sound(Some("sh"), Some("i")), Sound(Some("m"), Some("a")), Sound(None, Some("e")))
+      r.decompose("kanzen") === Array(Sound(Some("k"), Some("a")), Sound(Some("n"), None), Sound(Some("z"), Some("e")), Sound(Some("n"), None))
+      r.decompose("fab") must throwA(new IllegalArgumentException(s"Roman word needs to finish vowel: b"))
+    }
+
+    "get sokuon" in {
+      val r = Roman()
+      r.getSokuon("ty") === ("", "ty")
+      r.getSokuon("tty") === ("ッ", "ty")
+      r.getSokuon("tt") === ("ッ", "t")
+      r.getSokuon("tttt") === ("ッッッ", "t")
+    }
+
+    "convert" in {
+      val r = Roman()
+      r.convert(Array(Sound(None, Some("o")), Sound(Some("ttt"), Some("o")), Sound(Some("sh"), Some("i")), Sound(Some("m"), Some("a")), Sound(None, Some("e")))) === "オッットシマエ"
+      r.convert(Array(Sound(Some("k"), Some("a")), Sound(Some("n"), None), Sound(Some("z"), Some("e")), Sound(Some("n"), None))) === "カンゼン"
     }
   }
 } 
