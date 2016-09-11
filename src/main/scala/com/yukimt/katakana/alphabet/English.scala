e070276a7c5ee0031a677203bd9ba2d7db3d64f7
vagrant <vagrant@localhost.localdomain>
2016-09-11 10:58:13 +0000

    add convert method

--- src/main/scala/com/yukimt/katakana/alphabet/English.scala
+++ src/main/scala/com/yukimt/katakana/alphabet/English.scala
@@ -2,9 +2,11 @@ package com.yukimt.katakana.alphabet
 
 case object English extends Alphabet{
   val vowels = Set('a', 'i', 'u', 'e', 'o')
+  val shortVowels = Map('a'->"ア", 'i'->"イ", 'u'->"ウ", 'e'->"エ", 'o'->"オ", 'y'->"イ", 'w'->"ウ")
+  val longVowels = Map('a'->"エー", 'i'->"アイ", 'u'->"ウー", 'e'->"イー", 'o'->"オー", 'y'->"アイ", 'w'->"ウ")
+  val multiVowels = Map("ie"->"イー", "uy"->"アイ", "au"->"オー", "aw"->"オー", "eau"->"ユー", "eu"->"ユー", "io"->"イオ", "ou"->"アウ", "ye"->"イエ")
 
   val consonants = Map(
-    "" -> Seq("ア", "イ", "ウ", "エ", "オ"),
     "b"-> Seq("バ", "ビ", "ブ", "べ", "ボ"),
     "c" -> Seq("カ", "キ", "ク", "ケ", "コ"),
     "d" -> Seq("ダ", "ヂ", "ヅ", "デ", "ド"),
@@ -33,6 +35,7 @@ case object English extends Alphabet{
     "ph" -> Seq("ミャ", "ミィ", "ミュ", "ミェ", "ミョ"),
     "ght" -> Seq("ニャ", "ニィ", "ニュ", "ニェ", "ニョ"),
     "th" -> Seq("ピャ", "ピィ", "ピュ", "ピェ", "ピョ"),
+    "wh" -> Seq("ピャ", "ピィ", "ピュ", "ピェ", "ピョ"),
     "sc" -> Seq("リャ", "リィ", "リュ", "リェ", "リョ"))
 
   def decompose(str: String): Array[Sound] = {
@@ -60,7 +63,41 @@ case object English extends Alphabet{
   }
 
   def convert(sounds: Seq[Sound]): String = {
+    //convert vowel
+    sounds.zipWithIndex.map{
+      case (sound, index) =>
+        val kVowel = sound.vowel.map{v =>
+          if(v == "e" && index == sounds.size - 1 && sounds.size > 1){
+            //ignore e if sounds end with e and the size of sounds is more than 1
+            getDefaultVowel(sound.consonant)
+          } else if (true){
+            //if size of vowel > 1 and contained in multivowels keys, use it
+            //multirvowels + "r" -> longrvowel(remove "ー" if exists) + アー
+            //if size of vowel > 1 and not contained in multivowels keys,
+            //この下のは別々のifブロックにする
+            //“ie” + t => アイエット
+            //ia + l => アイアル
+            //oo: ウー bookとかッが入る場合はウ(dはッはいれない)
+            //ou + 子音 + “le”-> 「ア」
+            //ou + ghで終わり> 「ア」
+          } else if (true){
+            //longvowels if vowel + consonant(no overlapped) + [e, or, er, el, le]
+            //longvowels if vowel + ght
+            //longrvowels + "r" -> longrvowel(remove "ー" if exists) + アー
+            //基本複合母音のときは1文字目を英語読みにする。(yはアイ)
+          } else {
+            // use shortvowels
+            // if end with "r" -> アー
+          } 
+        }.getOrElse(getDefaultVowel(sound.consonant))
+    }
+    //get the first katakana in vowel and convert consonant to kantakana
     ""
+  }
+
+  def getDefaultVowel(consonant: Option[String]) = {
+    if(consonant.exists(c => c.endsWith("t") || c.endsWith("d"))) "オ"
+    else "ウ"
   }
 
   def vowelToInt(vowel: Char) = vowel match {
@@ -69,6 +106,7 @@ case object English extends Alphabet{
     case 'ウ' => 2
     case 'エ' => 3
     case 'オ' => 4
+    case 'ユ' => 5
   }
 
   def isHeadVowel(str: String) = {
@@ -78,10 +116,10 @@ case object English extends Alphabet{
         if(str.length > 1 && (str(1) == 'a' || str(1) == 'u' || str(1) == 'o')) false
         else true
       case 'w' =>
-        if(str.length > 1 && (vowels.contains(str(1)) || str(1) == 'y')) false
+        if(str.length > 1 && (vowels.contains(str(1)) || str(1) == 'y' || str(1) == 'h')) false
         else true
       case 'r' =>
-        if(str.length > 1 && (vowels.contains(str(1)) || str(1) == 'y')) false
+        if(str.length > 1 && (vowels.contains(str(1)) || str(1) == 'y' || str(1) == 'r')) false
         else true
       case _ => false
     }
